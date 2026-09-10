be43bf69d4e8be009533cb3da16f592e04c80dac
cococorentin24-create <cococorentin24@gmail.com>
2026-09-10 15:50:11 +0100

    Update nsi.py

--- nsi.py
+++ nsi.py
@@ -1,4 +1,4 @@
-# exercice 1-4
+# exercice 1-3
 print("bonjour")
 nom = input("Entrez votre nom : ")
 sexe = input("entrez votre sex (M ou F) : ").strip().upper()
@@ -12,3 +12,10 @@ age = int(input("Quel est votre âge ? "))
 age_2040 = age + 14
 print(f"Votre âge est {age}")
 print(f"Votre âge en 2040 sera {age_2040} ans.")
+# exercice 4
+a=int(input("Donner le premier entier : ?"))
+b=int(input("Donner le deuxieme entier : ?"))
+c=a//b
+r=a%b
+print("Leur question est ",c)
+print("Le reste de la division est ",r)
